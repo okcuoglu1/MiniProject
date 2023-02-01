34a7d5a4dcd8ac7447df48eb1d6e759e824beb7e
Cemil <okcuoglucemil@gmail.com>
2023-02-01 22:42:28 +0300

    Some class is changed

---
+++ src/AppoinmentApp/Appointment.java
@@ -0,0 +1,60 @@
+package AppoinmentApp;
+/*
+5-Randevu:
+        -id : otomatik üretilsin
+        -hasta adı,Doktor,randevu tarihi
+
+
+ */
+
+
+
+public class Appointment {
+
+    private static int counter=0;
+    private int id;
+    private String name;
+    private Doctor doctor;
+    private String date;
+
+    public Appointment(String name, Doctor doctor, String date) {
+        counter++;
+        this.id = counter;
+        this.name = name;
+        this.doctor = doctor;
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+
+
+
+
+}
